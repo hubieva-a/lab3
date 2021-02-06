2def59f10f00b2ab8ff80d7c8968f9aa17c4d417
hubieva-a <72037369+hubieva-a@users.noreply.github.com>
2021-02-06 09:02:53 +0300

    Delete PycharmProjects/lab3 directory

--- PycharmProjects/lab3/1.py
+++
@@ -1,9 +0,0 @@
-whatsyourname = input("What is your name?")
-howoldareyou = input("How old are you?")
-wheredoyoulive = input("Where do you live?")
-
-print("This is " + whatsyourname)
-print("(S)he is " + howoldareyou)
-print("(S)he lives in " + wheredoyoulive)
-
-
